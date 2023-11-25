14761436a7bd06e485382b00b34733ece59222fb
gabumwaingo <gabumwaingo@gmail.com>
2023-11-25 01:13:58 +0000

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create the readline interface
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Prompt user for their name
-console.log("Welcome to Holberton School, what is your name?");
-
-// Read user input
-rl.question('', (name) => {
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Close the program
-  console.log("This important software is now closing");
-  rl.close();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
